88f169c4da688906deaac95c3865f1cff3498e9b
Moarbue <83272310+Moarbue@users.noreply.github.com>
2022-12-05 09:33:48 +0100

    Calculate filtered data and plot to serial_port_plotter

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,23 @@
 
 #define LDR_PIN A0
 #define ADC_RESOLUTION 1024  // 10 bit
-#define MEASURE_INTERVAL 100 // ms
+#define MEASURE_INTERVAL 10 // ms
 #define SUPPLY_VOLTAGE 5.f
+#define BAUD_RATE 115200
 
 #define VOLT_TO_LUX(V) ((float)(23.4548121 * pow(M_E, 1.19824556*(V))))
 
-#define PRINT_INTERVAL 500 //ms
+#define FILTER_WEIGHT1 0.95f
+#define FILTER_WEIGHT2 (1.f - FILTER_WEIGHT1)
+
+#define PRINT_INTERVAL 15  //ms
 
 
 typedef struct {
   uint16_t adc;
   float voltage;
   float lux;
+  float luxf;
 } ldr_t;
 
 ldr_t ldr = {0};
@@ -23,7 +28,7 @@ unsigned long mills, last_measured, last_printed;
 void print_vals(void);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   while (!Serial);
 
   mills = last_measured = last_printed = millis();
@@ -39,6 +44,7 @@ void loop() {
     ldr.voltage = SUPPLY_VOLTAGE / (ADC_RESOLUTION - 1) * (float)ldr.adc;
     ldr.lux     = VOLT_TO_LUX(ldr.voltage); 
     ldr.lux     = ldr.lux > 999.f ? 999.f : ldr.lux;
+    ldr.luxf    = FILTER_WEIGHT1 * ldr.luxf + FILTER_WEIGHT2 * ldr.lux;
   }
 
   if (mills - last_printed >= PRINT_INTERVAL) {
@@ -50,13 +56,9 @@ void loop() {
 
 void print_vals(void)
 {
-    Serial.print("ADC = ");
-    Serial.print(ldr.adc);
-    Serial.print("    ");
-    Serial.print("V = ");
-    Serial.print(ldr.voltage, 3);
-    Serial.print("    ");
-    Serial.print("LUX = ");
-    Serial.print(ldr.lux, 3);
-    Serial.print("         \r");
+  Serial.write('$');
+  Serial.print(ldr.lux, 3);
+  Serial.write(' ');
+  Serial.print(ldr.luxf, 3);
+  Serial.write(';');
 }
